bc4e252dcdbf5c3f4bf53f621c0debd252652af6
Christoph Dorn <christoph@christophdorn.com>
2013-10-20 22:37:09 -0700

    test skeleton

---
+++ test/installer.js
@@ -0,0 +1,15 @@
+
+const PATH = require("path");
+const ASSERT = require("assert");
+const INSTALLER = require("../lib/installer");
+
+//const MODE = "test";
+const MODE = "write";
+
+const DEBUG = false;
+
+describe('installer', function() {
+
+
+
+});
