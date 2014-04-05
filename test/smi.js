7b388b030d54db9acbc3f8867d93618843a9893f
Christoph Dorn <christoph@christophdorn.com>
2014-04-04 18:41:07 -0700

    initial implementation

--- test/smi.js
+++ test/smi.js
@@ -1,6 +1,8 @@
 
-const PATH = require("path");
 const ASSERT = require("assert");
+const PATH = require("path");
+const FS = require("fs-extra");
+const DIRSUM = require("dirsum");
 const SMI = require("../lib/smi");
 
 //const MODE = "test";
@@ -8,8 +10,51 @@ const MODE = "write";
 
 const DEBUG = false;
 
+
 describe('smi', function() {
 
+	this.timeout(30 * 1000);
+	var tmpPath = PATH.join(__dirname, ".tmp");
+	if (FS.existsSync(tmpPath)) {
+		FS.removeSync(tmpPath);
+	}
 
+	var assetBasePath = PATH.join(__dirname, "assets");
+
+	FS.readdirSync(assetBasePath).forEach(function(filename) {
+		if (!/^\d+-([^\.]+)\.smi\.json$/.test(filename)) return;
+
+		var testName = filename.replace(/\.smi\.json$/, "");
+
+    	it(testName, function(callback) {
+
+			var resultPath = PATH.join(tmpPath, testName);
+
+    		function run(expectPath, callback) {
+	    		return SMI.install(resultPath, FS.readJsonSync(PATH.join(assetBasePath, filename)), function(err, info) {
+	    			if (err) return callback(err);
+					return DIRSUM.digest(resultPath, function(err, hashes) {
+						if (err) return callback(err);
+						var expectInfo = {
+							hashes: hashes,
+							info: info
+						};
+						if (MODE === "write") {
+							FS.outputFileSync(expectPath, JSON.stringify(expectInfo, null, 4));
+						} else {
+							ASSERT.deepEqual(expectInfo, FS.readJsonSync(expectPath));
+						}
+						return callback(null);
+					});
+	    		});
+    		}
+
+    		return run(PATH.join(assetBasePath, testName + ".expect.1.json"), function(err) {
+    			if (err) return callback(err);
+	    		return run(PATH.join(assetBasePath, testName + ".expect.2.json"), callback);
+    		});
+	    });
+	});
 
 });
+
